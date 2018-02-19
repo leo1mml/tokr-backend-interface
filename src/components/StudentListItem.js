2f4bcb0b98c2618099240029699cd72cc961631d
leo1mml <mml_leo@hotmail.com>
2018-02-19 20:42:03 -0300

    main styles for tables added

--- src/components/StudentListItem.js
+++ src/components/StudentListItem.js
@@ -2,10 +2,18 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 
-const StudentListItem = ({_id, name, email, cellPhone, birthDate, cpf, address}) => (
-    <div>
-        <p>{name} cpf: {cpf}</p>
-    </div>
+const StudentListItem = ({_id, name, email, cellPhone, birthDate, cpf, address, instruments, musicStyles}) => (
+    
+    <tr>
+        <td>{name}</td>
+        <td>{cpf}</td>
+        <td>{email}</td>
+        <td>{address}</td>
+        <td>{instruments.map((instrument) => {
+                return `${instrument}, `
+            })}</td>
+        <td>{musicStyles}</td>
+    </tr>
 )
 
 export default StudentListItem
--- src/components/StudentsList.js
+++ src/components/StudentsList.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { connect } from 'react-redux';
 import {startFetchStudents} from '../actions/students'
 import StudentListItem from './StudentListItem'
+import '../styles/tables/commonTable.css'
+import '../styles/titles/list-titles.css'
+import '../styles/top-widget-container/top-widget.css'
 
 export class StudentsList extends React.Component{
 
@@ -11,20 +14,34 @@ export class StudentsList extends React.Component{
 
     render() {
         return (
-            <div>
-                <h1>Lista de alunos</h1>
-                <button>Adicionar Aluno</button>
-                <p>Buscar(nome/cpf)</p> 
-                <input type="text" value=""/>
-                {
-                    this.props.students.length === 0 ? (
-                        <p>Sem alunos</p>
-                    ) : (
+            <div className="list-container">
+                <h1 className="list-title">Alunos Tokr</h1>
+                <div className="list-header">
+                    <p>
+                        Buscar: 
+                        <input type="text"/>
+                    </p>
+                    <button className="add-button">Adicionar Aluno</button>
+                </div>
+                <table id="customers">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>CPF</th>
+                            <th>E-mail</th>
+                            <th>Endereço</th>
+                            <th>Instrumentos</th>
+                            <th>Estilos Musicais</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {
                         this.props.students.map((student) => {
                             return <StudentListItem key={student._id} {...student} />;
                         })
-                    )
-                }
+                    }
+                    </tbody>
+                </table>
             </div>
         )
     }
